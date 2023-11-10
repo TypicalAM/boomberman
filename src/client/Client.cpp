fc8920e90bcf8a97b854254f8b6107f5aad98b94
bynajmniejIgnor <igor.szczepaniak.02@gmail.com>
2023-11-10 16:16:19 +0100

    Updated client

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -1,21 +1,75 @@
 #include <iostream>
+#include <cstring>
 #include "Client.h"
 #include "raylib.h"
+#include <array>
 
 void Client::Run() {
+    Map map(25);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("HAHAHAHAHAHAHAHAHAHAHAHAHAAHAH!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(LIGHTGRAY);
+        map.drawMap(this);
         EndDrawing();
+    }
+}
+
+int Boomerman::getBoomermanPosX(){
+    return this->player_x;
+}
+int Boomerman::getBoomermanPosY(){
+    return this->player_y;
+}
+
+void Map::updateMap(const std::vector<std::array<int, 2>>& player_positions) {
+    for(const auto& pos : player_positions){
+        int x=pos[0];
+        int y=pos[1];
+
+        this->map[x][y]=2;
+    }
+}
+
+void Map::drawMap(Client *client) {
+    int offset = int((size*4)/3);
+    int start_x = int(client->getDimension("width")/2 - (cols/2)*offset);
+    int start_y = int(client->getDimension("height")/2 - (rows/2)*offset);
+
+    auto color = GRAY;
+    for (int x = 0; x < this->cols; x++) {
+        for (int y = 0; y < this->rows; y++) {
+            if(this->map[x][y]==1) color = BLACK;
+            else if(this->map[x][y]==2) color = RED;
+            else color = GRAY;
+            DrawRectangle(x * offset + start_x, y * offset + start_y, this->size, this->size, color);
+        }
+    }
+}
+
+Map::Map(int size){
+    this->size = size;
+    memset( this->map, 0, sizeof(this->map) );
+
+    for (int x = 0; x < this->cols; x++) {
+        for (int y = 0; y < this->rows; y++) {
+            if(x==0 || x==this->cols-1 || y==0 || y==this->rows-1) this->map[x][y]=1;
+        }
     }
 }
 
 Client::Client(int width, int height) {
     this->width = width;
     this->height = height;
+
     InitWindow(width, height, "Boomberman client");
     SetTargetFPS(60);
+}
+
+int Client::getDimension(const std::string& dimension) const {
+    if(dimension=="height") return this->height;
+    else if(dimension=="width") return this->width;
+    else return -1;
 }
 
 Client::~Client() {
--- src/client/Client.h
+++ src/client/Client.h
@@ -1,6 +1,10 @@
 #ifndef BOOMBERMAN_CLIENT_H
 #define BOOMBERMAN_CLIENT_H
 
+#include <vector>
+
+#define MAP_WIDTH 18
+#define MAP_HEIGHT 10
 
 class Client {
 private:
@@ -8,11 +12,38 @@ private:
     int height;
 
 public:
-    void Run();
-
     Client(int width, int height);
 
+    void Run();
+    int getDimension(const std::string& dimension) const;
+
     ~Client();
+};
+
+class Boomerman{
+private:
+    int player_x;
+    int player_y;
+    int health;
+public:
+    Boomerman(int player_x, int player_y, int health);
+    int getBoomermanPosX();
+    int getBoomermanPosY();
+};
+
+class Map{
+private:
+    int map[MAP_WIDTH][MAP_HEIGHT]{};
+    int cols = int(sizeof(map)/ sizeof(map[-1]));
+    int rows= int(sizeof(map[-1])/ sizeof(int));
+    int size;
+public:
+    explicit Map(int size);
+
+    void updateMap(const std::vector<std::array<int, 2>>& player_positions);
+    void drawMap(Client *client);
+
+
 };
 
 #endif
