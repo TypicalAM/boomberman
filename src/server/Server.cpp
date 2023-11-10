b3761e7ed1b1a73c53199eb7ce29389577bc581d
TypicalAM <tpam_piaseczny@protonmail.com>
2023-11-10 09:49:55 +0100

    Basic connection handling

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <random>
+#include <chrono>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include "Sever.h"
 
 void Server::Run() {
-    std::cout << "Serving at port: " << this->port << std::endl;
+    std::cout << "Starting server" << std::endl;
+    sockaddr_in localAddress{
+        .sin_family = AF_INET,
+        .sin_port = htons(this->port),
+        .sin_addr = htonl(INADDR_ANY),
+    };
+
+    int servSock = socket(PF_INET, SOCK_STREAM, 0);
+    int one = 1;
+    setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+
+    if(bind(servSock, (sockaddr*) &localAddress, sizeof(localAddress)))
+        throw std::runtime_error("can't bind");
+
+    listen(servSock, 1);
+    std::cout << "Serving on port: " << this->port << std::endl;
+    while (true) {
+        int clientSock = accept(servSock, nullptr, nullptr);
+        std::cout << "Got client!: " << clientSock << std::endl;
+        break;
+    }
 }
 
 Server::Server(int port) {
