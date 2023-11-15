0f6fe5b15aa26d0a4830fce238dec0fdfee05613
TypicalAM <tpam_piaseczny@protonmail.com>
2023-11-15 14:55:51 +0100

    Better test client functionality

--- test-client/main.go
+++ test-client/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"io"
 	"log"
 	"net"
-	"strings"
-	"time"
+
+	"github.com/TypicalAM/boomberman/test-client/msg"
+	"google.golang.org/protobuf/proto"
 )
 
 const serverAddr = "localhost:2137"
@@ -21,41 +20,39 @@ func main() {
 	}
 	defer conn.Close()
 
-	log.Println("Operating for 5 seconds...")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	go readLoop(ctx, conn)
-	<-ctx.Done()
+	msgs := make(chan *msg.GameMessage)
+	go readLoop(conn, msgs)
+	actLoop(msgs)
 }
 
-func bytesToHex(bytes []byte) string {
-	b := strings.Builder{}
-	for _, msgb := range bytes {
-		b.WriteString(fmt.Sprintf("0x%02x", msgb))
-	}
-	return b.String()
-}
-
-func readLoop(ctx context.Context, conn net.Conn) {
+func readLoop(conn net.Conn, msgs chan *msg.GameMessage) {
 	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Context done, exiting read loop")
+		bytes := make([]byte, 512)
+		n, err := conn.Read(bytes)
+		if err != nil && !errors.Is(err, io.EOF) {
+			log.Fatal(err)
+		}
+
+		if n == 0 {
+			continue
+		}
+
+		var msg msg.GameMessage
+		if err := proto.Unmarshal(bytes[:n], &msg); err != nil {
+			log.Println("Failed to unmarshal message:", err)
 			return
+		}
+		msgs <- &msg
+	}
+}
 
-		default:
-			bytes := make([]byte, 256)
-			n, err := conn.Read(bytes)
-			if err != nil && !errors.Is(err, io.EOF) {
-				log.Fatal(err)
-			}
-
-			if n == 0 {
-				continue
-			}
-
-			fmt.Printf("Received %d bytes: %s\n", n, bytesToHex(bytes))
+func actLoop(msgs chan *msg.GameMessage) {
+	for gMsg := range msgs {
+		log.Println("New message!")
+		log.Println(gMsg.MessageType)
+		if gMsg.MessageType == msg.MessageType_GAME_JOIN {
+			gj := gMsg.GetGameJoin()
+			log.Println("Game join:", gj.Name, gj.Color, gj.You)
 		}
 	}
 }
