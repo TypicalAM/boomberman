843ac81873bb33a5ea823e3c668897330b486cde
TypicalAM <tpam_piaseczny@protonmail.com>
2023-11-22 21:58:12 +0100

    Add a cmake file for boost

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -2,11 +2,25 @@
 #include <iostream>
 #include "server/Server.h"
 #include "client/Client.h"
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 const int screenWidth = 800;
 const int screenHeight = 450;
 
+void init_logging() {
+    boost::log::add_common_attributes();
+}
+
 int main(int argc, char *argv[]) {
+    init_logging();
+    BOOST_LOG_TRIVIAL(info) << "Hello bombaman";
+
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " client/server" << std::endl;
         return 1;
